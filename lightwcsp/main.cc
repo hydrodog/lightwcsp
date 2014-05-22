4098e20374a85534576531d0875b31aba606fc45
hydrodog <dkruger@155.246.69.202>
2014-05-22 17:28:28 -0400

    First, simple main file with a couple of placeholder objects like the logger and the Config.

---
+++ lightwcsp/main.cc
@@ -0,0 +1,25 @@
+/*
+ * main.cc
+ *
+ *  The main for the Lightweight C++ Server Pages Project
+ *  A fast, lightweight web server implemented in C++
+ *  Similar to JSP, with integrated security
+ *
+ *  Created on: May 22, 2014
+ *      Author: dkruger
+ */
+#include <iostream>
+using namespace std;
+#if 0
+  // not implemented yet, get busy people!
+  Logger logger;
+  Config config; // create a config object
+#endif
+int main() {
+  //config.load("lightwcsp.conf");
+
+  exit(0);
+}
+
+
+
