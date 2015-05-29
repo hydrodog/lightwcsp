914e92784a1bdbf5c847e638fb4e01e5d5d6e604
mabrunoro <maaubt@gmail.com>
2015-05-28 20:13:17 -0400

    Client build html file using the numbers given by the server and files vec.dat and fixed.dat
    Files fixed.dat and vec.dat are generated by mdata.cc

--- hydroprot/client.cc
+++ hydroprot/client.cc
@@ -13,8 +13,8 @@
 // #include <string>
 #include <cstring>
 #include <iomanip>
-#include <stdio.h>
-#include <regex>
+// #include <cstdio>
+#include <fstream>
 
 using namespace std;
 
@@ -97,6 +97,61 @@ int main(int argc,char *argv[])
 
 	}
 	freeaddrinfo(result);
+
+	// File reconstruction
+	ifstream fixed("fixed.dat");
+	ifstream vec("vec.dat");
+
+	if((!fixed.is_open())||(!vec.is_open()))
+	{
+		cout << "Unable to open files" << endl;
+		return 0;
+	}
+
+	ofstream file("rec.html");
+
+	string vline;
+	string fline;
+	int cline = 0;
+	int i = 0;
+
+	while(getline(vec,vline))
+	{
+		if(vline[0]=='-')
+		{
+			if(i)
+				file << (fline.c_str()+i) << endl;
+			while(getline(fixed,fline))
+				file << fline << endl;
+			break;
+		}
+		else
+		{
+			int l,c,n;
+			sscanf(vline.c_str(),"%d,%d,%d",&l,&c,&n);
+			if((cline < l)&&i)
+			{
+				file << (fline.c_str()+i) << endl;
+				cline++;
+				i = 0;
+			}
+			while(cline < l)
+			{
+				getline(fixed,fline);
+				file << fline << endl;
+				cline++;
+				i = 0;
+			}
+			if(cline==l)
+			{
+				if(!i)
+					getline(fixed,fline);
+				for(; i<c; i++)
+					file << fline[i];
+				file << v[n];
+			}
+		}
+	}
 
 	return 0;
 }
