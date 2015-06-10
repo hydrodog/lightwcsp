e60e4f7c82a8a4f4e6bddd37c11fa1ff27af55e3
mabrunoro <maaubt@gmail.com>
2015-06-10 14:02:47 -0400

    Changed builder and created inc/

--- hydroprot/builder.cc
+++ hydroprot/builder.cc
@@ -16,11 +16,10 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
-	bool inheritance = false;
-	ifstream child;
+	// bool inheritance = false;
+	ifstream child(argv[1]);
 
-	ifstream base(argv[1]);
-	if(!base.is_open())
+	if(!child.is_open())
 	{
 		cout << "Error opening file " << argv[1] << endl;
 		return 0;
@@ -38,63 +37,56 @@ int main(int argc, char *argv[])
 	regex block("\\{\\%\\s*block\\s*(\\w+)\\s*\\%\\}\\s*([\\s\\S]*?)\\s*\\{\\%\\s*endblock\\s*\\%\\}");
 	regex attr("\\{\\%\\s*attr\\s*(\\w+)=*(\"*.+?\"*)\\s*\\%\\}");
 	
-	if(argc < 3)
-		cout << "No child file. Inheritance blocks will be ignored." << endl;
-	else
+	// bool dynamic = false;
+	ifstream base;
+	if(!getline(child,line))
 	{
-		child.open(argv[2]);
-		inheritance = child.is_open();
-		if(inheritance)
+		cout << "File is empty" << endl;
+		return 0;
+	}
+	if(regex_search(line,sm,regex("(extends )(\\\"+)\\s*(.+)\\s*(\\\"+)")))
+	{
+		base.open("inc/"+sm[3].str());
+		if(!base.is_open())
 		{
-			string line;
-			getline(child,line);
-			regex_search(line,sm,regex("(extends )(\\\"+)\\s*(.+)\\s*(\\\"+)"));
-			line = sm[3].str();
-			int aux = string(argv[1]).find_last_of('/');
-			if(aux==string::npos)
-				aux = -1;
-			if(line.compare((argv[1])+aux+1))
+			cout << "Error opening base file" << endl;
+			child.close();
+			base.open(argv[1]);
+		}
+		else
+		{
+			string aux;
+			// inheritance = true;
+			while(getline(child,line))
 			{
-				cout << "Child does not match base file. Inheritance blocks will be ignored." << endl;
-				inheritance = false;
-			}
-			else
-			{
-				string aux;
-				inheritance = true;
-				while(getline(child,line))
+				if(regex_search(line,sm,attr))
 				{
-					if(regex_search(line,sm,attr))
+					aux = sm[1].str();
+					mp[aux] = regex_replace(regex_replace(sm[2].str(),escaped2,"%$&"),escaped1,"\\$&");
+					aux = "";
+				}
+				else
+				{
+					aux = aux+line;
+					if(regex_search(aux,sm,block))
 					{
 						aux = sm[1].str();
 						mp[aux] = regex_replace(regex_replace(sm[2].str(),escaped2,"%$&"),escaped1,"\\$&");
 						aux = "";
 					}
-					else
-					{
-						aux = aux+line;
-						if(regex_search(aux,sm,block))
-						{
-							aux = sm[1].str();
-							mp[aux] = regex_replace(regex_replace(sm[2].str(),escaped2,"%$&"),escaped1,"\\$&");
-							aux = "";
-						}
-					}
 				}
 			}
-		}
-		else
-		{
-			cout << "Error opening child file. Inheritance will be ignored." << endl;
-			inheritance = false;
+			child.close();
 		}
 	}
+	else
+	{
+		child.close();
+		base.open(argv[1]);
+	}
 
-	for(map<string,string>::iterator i = mp.begin();i!=mp.end();i++)
-		cout << i->first << ' ' << i->second << endl;
-
-	bool dynamic = false;
 	regex_search(string(argv[1]),sm,regex("(\\w+\\/)*(\\w+)(\\.\\w+)"));
+	
 	ofstream file(sm[2].str()+".cc");
 
 	file << "class " << sm[2].str() << " : public HttpServlet"
@@ -106,7 +98,7 @@ int main(int argc, char *argv[])
 		line = regex_replace(line,escaped1,"\\$&");
 		if(regex_search(line,sm,variable))
 		{
-			dynamic = true;
+			// dynamic = true;
 			if(sm.prefix().length() > 0)
 				file << "\t\tb.append(\"" << (regex_replace(sm.prefix().str(),escaped2,"%$&")) << "\");" << endl;
 			if(sm.suffix().length() > 0)
@@ -122,7 +114,7 @@ int main(int argc, char *argv[])
 		}
 		else if(regex_search(line,sm,cppcode))
 		{
-			dynamic = true;
+			// dynamic = true;
 			if(sm.prefix().length() > 0)
 				file << "\t\tb.append(\"" << regex_replace(sm.prefix().str(),escaped2,"%$&") << "\");" << endl;
 			if(sm.suffix().length() > 0)
@@ -139,10 +131,9 @@ int main(int argc, char *argv[])
 				file << "\t\tb.append(\"" << regex_replace(sm.prefix().str(),escaped2,"%$&")
 					<< regex_replace(sm.suffix().str(),escaped2,"%$&") << "\\n\");" << endl;
 		}
-		else if(regex_search(line,sm,substitution)&&inheritance)
+		else if(regex_search(line,sm,substitution))
 		{
 			string aux;
-			// cout << sm.prefix().str() << endl;
 			file << "\t\tb.append(\"" << regex_replace(sm.prefix().str(),escaped2,"%$&") << "\");" << endl;
 			if(regex_search(line,sm,attr))
 				aux = mp[sm[1].str()];
@@ -169,9 +160,10 @@ int main(int argc, char *argv[])
 			file << "\t\tb.append(\"" << regex_replace(line,escaped2,"%$&") << "\\n\");" << endl;
 		}
 	}
+
 	file << "\t}\n};";
 	file.close();
-	child.close();
+	// child.close();
 	base.close();
 	return 0;
 }
