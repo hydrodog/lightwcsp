5a1319fb040ada83e027fc4248ef4f14a0497ad4
mabrunoro <maaubt@gmail.com>
2015-06-22 11:44:37 -0400

    Started itoa tests

--- hydroprot/convert_test.cc
+++ hydroprot/convert_test.cc
@@ -8,9 +8,14 @@ using namespace std;
 int main(int argc, char* argv[])
 {
 	int n = atoi(argv[1]);
-	double d = 0.1;
+	// double d = 0.1;
+	// char *s;
+	// for(int i=0;i<n;i++)
+	// 	s = convert((d+=(0.1*i)),pr);
+	// return 0;
+	int d = 1;
 	char *s;
 	for(int i=0;i<n;i++)
-		s = convert((d+=(0.1*i)),pr);
+		s = itoa(d+=(-10*i));
 	return 0;
 }
--- hydroprot/itoa_test.cc
+++
@@ -1,7 +0,0 @@
-#include <_itoa.c>
-using namespace std;
-
-int main()
-{
-	return 0;
-}
